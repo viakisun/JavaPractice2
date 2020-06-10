bf665f4a8a82c177f70deb93ba5b574a002c8004
viakisun <kisun@viasofts.com>
2020-06-10 10:53:30 +0900

    practice #1 - temperature converter

--- src/JavaPractice.java
+++ src/JavaPractice.java
@@ -2,7 +2,13 @@
 public class JavaPractice {
 
 	public static void main(String[] args) {
-
+		runTemperatureConverter();
 	}
 
+	private static void runTemperatureConverter() {
+		float celciusValue = 31.5f;
+		float farenheitValue = Util.convertCelciusToFarenheit(celciusValue);
+		System.out.printf("celcius : %f%n", celciusValue);
+		System.out.printf("farenheit : %f%n", farenheitValue);
+	}
 }
---
+++ src/Util.java
@@ -0,0 +1,7 @@
+
+public class Util {
+	public static float convertCelciusToFarenheit(float celciusValue) {
+		// Formula : (0¡ÆC ¡¿ 9/5) + 32 = 32¡ÆF
+		return celciusValue * 9 / 5.0f + 32; 
+	}
+}
