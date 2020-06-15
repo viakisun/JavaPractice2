e599cddcabbe98a3a572834c28ae836571f42a1c
viakisun <kisun@viasofts.com>
2020-06-15 10:14:38 +0900

    bug fixed:negative deposit/withdraw error

--- src/com/via/practice/BankAccount.java
+++ src/com/via/practice/BankAccount.java
@@ -1,4 +1,14 @@
 package com.via.practice;
+
+/**
+* The BankAccount program implements an application that
+* can create bank account and deposit, withdraw money.
+*
+* @author  Vincent Park
+* @version 0.1
+* @since   2020-06-12 
+*/
+
 public class BankAccount {
 	private String mAccount;
 	private String mAccountHolder;
@@ -10,22 +20,29 @@ public class BankAccount {
 		mAmount = amount;
 	}
 
-	public void deposit(int amount) {
+	public boolean deposit(int amount) {
+		if (!isPositiveAmount(amount)) return false;
+		
 		mAmount += amount;
 
 		String strFormat = "%d 원 입금합니다.%n";
 		System.out.printf(strFormat, amount);
 
 		printAmount();
+		
+		return true;
 	}
 
-	public void withDraw(int amount) {
+	public boolean withDraw(int amount) {
+		if (!isPositiveAmount(amount)) return false;
+		
 		mAmount -= amount;
 
 		String strFormat = "%d 원 출금합니다.%n";
 		System.out.printf(strFormat, amount);
 
 		printAmount();
+		return true;
 	}
 
 	private void printAmount() {
@@ -36,5 +53,15 @@ public class BankAccount {
 	public void printStatus() {
 		String strFormat = "계좌 %s (예금주:%s)%n잔액: %d 원%n";
 		System.out.printf(strFormat, mAccount, mAccountHolder, mAmount);
+	}
+	
+	private boolean isPositiveAmount(int amount)
+	{
+		if (amount < 0) {
+			System.out.println("[에러] 금액은 음수를 입력할 수 없습니다.");
+			return false;
+		}
+		
+		return true;
 	}
 }
