ea656d6c17c7742cfac049621a560382d8a129bd
viakisun <kisun@viasofts.com>
2020-06-30 12:03:34 +0900

    update conventions

--- src/com/via/practice/BankAccount.java
+++ src/com/via/practice/BankAccount.java
@@ -4,13 +4,13 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 /**
-* The BankAccount program implements an application that
-* can create bank account and deposit, withdraw money.
-*
-* @author  Vincent Park
-* @version 0.1
-* @since   2020-06-12 
-*/
+ * The BankAccount program implements an application that can create bank
+ * account and deposit, withdraw money.
+ *
+ * @author Vincent Park
+ * @version 0.1
+ * @since 2020-06-12
+ */
 
 public class BankAccount {
 	private String mAccount;
@@ -24,20 +24,23 @@ public class BankAccount {
 	}
 
 	public boolean deposit(int amount) {
-		if (!isPositiveAmount(amount)) return false;
-		
+		if (!isPositiveAmount(amount))
+			return false;
+
 		mAmount += amount;
 		System.out.printf(CommonMessage.MSG_BANK_DEPOSIT, convertToDecimalFormat(amount));
 
 		printAmount();
-		
+
 		return true;
 	}
 
 	public boolean withDraw(int amount) {
-		if (!isPositiveAmount(amount)) return false;
-		if (!canWithdraw(amount)) return false;
-		
+		if (!isPositiveAmount(amount))
+			return false;
+		if (!canWithdraw(amount))
+			return false;
+
 		mAmount -= amount;
 		System.out.printf(CommonMessage.MSG_BANK_WITHDRAW, convertToDecimalFormat(amount));
 
@@ -52,31 +55,29 @@ public class BankAccount {
 	public void printStatus() {
 		System.out.printf(CommonMessage.MSG_BANK_STATUS, mAccount, mAccountHolder, convertToDecimalFormat(mAmount));
 	}
-	
+
 	public String getAccountName() {
 		return mAccount;
 	}
-	
-	private boolean isPositiveAmount(int amount)
-	{
+
+	private boolean isPositiveAmount(int amount) {
 		if (amount < 0) {
 			System.out.println(CommonMessage.MSG_BANK_ERROR_NEGATIVE);
 			return false;
 		}
-		
+
 		return true;
 	}
-	
-	private boolean canWithdraw(int amount)
-	{
+
+	private boolean canWithdraw(int amount) {
 		if (mAmount - amount < 0) {
 			System.out.println(CommonMessage.MSG_BANK_ERROR_NOT_ENOUGH);
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	private String convertToDecimalFormat(int amount) {
 		NumberFormat nf = NumberFormat.getNumberInstance(Locale.KOREA);
 		nf.setMaximumFractionDigits(3);
